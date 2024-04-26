ac56dc77b9a3f5e16e2fe955a85b2799341977c0
kapii321 <kacper.sochacki003@gmail.com>
2024-04-27 01:13:54 +0200

    simpler model, using train, validation and testing datasets, accuracy 98-99%

--- model.py
+++ model.py
@@ -3,99 +3,39 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-def activation_layer(activation: str='relu', alpha: float=0.1, inplace: bool = True):
-    if activation == 'relu':
-        return nn.ReLU(inplace=inplace)
-    elif activation == 'leaky_relu':
-        return nn.LeakyReLU(negative_slope=alpha, inplace=inplace)
 
-
-class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, skip_conv=True, dropout=0.2,
-                 activation='leaky_relu', padding=1):
-        super(ResidualBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.activation1 = activation_layer(activation)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, stride=1,
-                               padding=padding)  # Stride is always 1
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        self.dropout = nn.Dropout(p=dropout)
-
-        # Adjusting the shortcut connection to match the dimensions of the main path
-        if skip_conv:
-            if stride != 1 or in_channels != out_channels:
-                self.shortcut = nn.Sequential(
-                    nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride),  # Adjust spatial dimensions
-                    nn.BatchNorm2d(out_channels)
-                )
-            else:
-                self.shortcut = nn.Identity()  # Identity mapping if dimensions match
-        else:
-            self.shortcut = nn.Identity()  # No skip connection
-
-        self.activation2 = activation_layer(activation)
-
-    def forward(self, x):
-        jump = self.shortcut(x)  # Apply shortcut connection
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.activation1(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        out += jump  # Perform element-wise addition with shortcut connection
-
-        out = self.activation2(out)
-
-        out = self.dropout(out)
-
-        return out
 
 
 class CNN(nn.Module):
 
-    def __init__(self,num_authors: int, activation: str='leaky_relu', dropout: float=0.2):
+    def __init__(self):
         super(CNN, self).__init__()
 
-        self.rb1 = ResidualBlock(3,16, skip_conv=True, stride=1, activation=activation, dropout=dropout)
-        self.rb2 = ResidualBlock(16,32, skip_conv=True, stride=2, activation=activation, dropout=dropout)
-        self.rb3 = ResidualBlock(32,64, skip_conv=True, stride=2, activation=activation, dropout=dropout)
-        self.rb4 = ResidualBlock(64,64, skip_conv=True, stride=1, activation=activation,dropout=dropout)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.activation = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.5)
+        self.fc1 = nn.Linear(in_features=130944, out_features=512)
+        self.fc2 = nn.Linear(in_features=512, out_features=8)
+        self.flatten = nn.Flatten()
 
-        self.lstm = nn.LSTM(64,128, bidirectional=True, num_layers=1, batch_first=True)
-        self.lstm_dropout = nn.Dropout(p=dropout)
-
-        self.output = nn.Linear(256, num_authors)
-
-    def forward(self, images: torch.Tensor) -> torch.Tensor:
-
-        #normalization if needed
-        '''
-        images_float = images / 255.0
-        images_float = images_float.permute(0,3,1,2)
-        '''
-
-        #change images to images_float if using normalization here
-        x = self.rb1(images)
-        x = self.rb2(x)
-        x = self.rb3(x)
-        x = self.rb4(x)
-
-        x = x.reshape(x.size(0), -1, x.size(1))
-
-        x, _ = self.lstm(x)
-        x = self.lstm_dropout(x)
-        x = self.output(x)
-
-        # was log_softmax but not needed for crossEntropyLoss, it is used only for
-        # CTCloss and we dont need it for author recognition, see if regular softmax works okay
-        # also test without softmax activation here, CrossEntropy uses it internally automatically
+    def forward(self, x):
+        x = self.activation(self.conv1(x))
+        x = self.maxpool(x)
+        x = self.activation(self.conv2(x))
+        x = self.maxpool(x)
+        x = self.dropout(x)
 
 
-        #dim =2 because of reshaping for lstm, remove if doesnt work properly and dont use softmax here
-        x = F.softmax(x, dim=2)
+        x = self.flatten(x)
+
+
+        x = self.activation(self.fc1(x))
+        x = self.dropout(x)
+
+        x = self.fc2(x)
 
         return x
+
+
--- train.py
+++ train.py
@@ -24,10 +24,10 @@ for folder in subFolder:
 
 
 class DataPreprocessor(Dataset):
-    def __init__(self, features, labels, num_classes, transform=None):
+    def __init__(self, features, labels,  transform=None):
         self.features = features
         self.labels = labels
-        self.num_classes = num_classes
+        #self.num_classes = num_classes
         self.transform = transform
 
     def __getitem__(self, item):
@@ -36,33 +36,37 @@ class DataPreprocessor(Dataset):
         if self.transform:
             image = self.transform(image)
 
-        target = torch.zeros(self.num_classes)
-        target[label] = 1
+        return image, label
 
-        return image, target
+       # target = torch.zeros(self.num_classes)
+        #target[label] = 1
+
+
 
     def __len__(self):
         return len(self.labels)
 
 
-train_images, val_images, train_labels, val_labels = train_test_split(images, labels, test_size=0.2, stratify=labels,
-                                                                      random_state=42)
+train_images, temp_images, train_labels, temp_labels = train_test_split(images, labels, test_size=0.2, stratify=labels, random_state=42)
+val_images, test_images, val_labels, test_labels = train_test_split(temp_images, temp_labels, test_size=0.5, stratify=temp_labels, random_state=42)
 
 data_transform = transforms.Compose([
     transforms.ToTensor()
 ])
 
-train_dataset = DataPreprocessor(train_images, train_labels, num_classes=len(subFolder), transform=data_transform)
-val_dataset = DataPreprocessor(val_images, val_labels, num_classes=len(subFolder), transform=data_transform)
+train_dataset = DataPreprocessor(train_images, train_labels,  transform=data_transform)
+val_dataset = DataPreprocessor(val_images, val_labels,  transform=data_transform)
+test_dataset = DataPreprocessor(test_images, test_labels,  transform=data_transform)
 
 train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
 
-model = CNN(num_authors=8).to(device)
+model = CNN().to(device)
 print(model.train())
 
 learning_rate = 0.001
-epoch = 100
+epoch = 50
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 criterion = nn.CrossEntropyLoss()
 
@@ -72,13 +76,14 @@ for i in range(epoch):
         image = image.to(device)
         target = target.to(device)
 
-        target = target.long()
 
         optimizer.zero_grad()
         output = model(image)
         loss = criterion(output, target)
         loss.backward()
         optimizer.step()
+
+
 
     print(f'Epochs: {i + 1} out of {epoch} || Training Loss: {loss.item()}')
 
@@ -95,4 +100,20 @@ for i in range(epoch):
             total_correct += (predicted == target).sum().item()
             total_samples += target.size(0)
         accuracy = total_correct / total_samples
-        print(f'Accuracy: {accuracy}')
+        print(f'Validation Accuracy: {accuracy}')
+
+# Testing (after training)
+model.eval()
+with torch.no_grad():
+    total_correct = 0
+    total_samples = 0
+    for image, target in test_loader:
+        image = image.to(device)
+        target = target.to(device)
+
+        output = model(image)
+        _, predicted = torch.max(output, 1)
+        total_correct += (predicted == target).sum().item()
+        total_samples += target.size(0)
+    accuracy = total_correct / total_samples
+    print(f'Testing Accuracy: {accuracy}')
